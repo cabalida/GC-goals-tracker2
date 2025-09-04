c8af8ee094677882b727039ff33f6006f393a080
cabalida <gilcabalida570@gmail.com>
2025-09-04 10:38:58 +0800

    Firebase Configuration

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,17 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Optionally import the services that you want to use
-// import {...} from 'firebase/auth';
-// import {...} from 'firebase/database';
-// import {...} from 'firebase/firestore';
-// import {...} from 'firebase/functions';
-// import {...} from 'firebase/storage';
-
-// Initialize Firebase
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+  apiKey: "AIzaSyDCFuQDLcBTuconNRW_tBOf5_XOBB7U9mQ",
+  authDomain: "gc-goal-tracker.firebaseapp.com",
+  projectId: "gc-goal-tracker",
+  storageBucket: "gc-goal-tracker.firebasestorage.app",
+  messagingSenderId: "128959289563",
+  appId: "1:128959289563:web:bd1d6caf7f9a771f0454fe"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
